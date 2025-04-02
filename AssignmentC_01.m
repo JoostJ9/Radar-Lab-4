69b65fe8bdcd43eb10135facd4d10dc452b50b3b
ADAALMAN <Adaalman@outlook.com>
2025-04-02 14:58:15 +0200

    added some images

--- AssignmentC_01.m
+++ AssignmentC_01.m
@@ -2,8 +2,9 @@
 figure;
 fc = 79e9
 lambda = (3e8/fc)
-theta = -90:0.1:90
-ang_res = 0.886.*lambda./(4.*2.*lambda./2.*cos(theta))
-plot(theta, ang_res)
+theta = -89:0.1:89
+ang_res = 0.886.*lambda./(4.*2.*lambda./2.*cos(theta*pi/180))
+plot(theta, ang_res*180/pi)
 xlabel("Angle (\circ)")
 ylabel("Angular resolution (\circ)")
+xlim([-80,80])
